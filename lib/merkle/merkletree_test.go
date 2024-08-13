c6dd9cb159f3c183caf8cef47d06f20e8dadc58f
Laura Hetz <17835323+laurahetz@users.noreply.github.com>
2024-08-13 17:49:07 +0200

    Test cases for Merkle Tree Generation and Proof Validation

--- lib/merkle/merkletree_test.go
+++ lib/merkle/merkletree_test.go
@@ -1,7 +1,9 @@
 package merkle
 
 import (
+	"hash/adler32"
 	"log"
+	"math"
 	"testing"
 
 	"github.com/si-co/vpir-code/lib/utils"
@@ -30,5 +32,40 @@ func BenchmarkNew(b *testing.B) {
 		if err != nil {
 			panic(err)
 		}
+	}
+}
+
+// Code from MerkleTree.go NewUsing() to test mapping from data entry to node index
+func TestTreeGen(t *testing.T) {
+
+	// fix key for debugging purposes
+	rng := utils.NewPRG(&utils.PRGKey{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	// rng := utils.RandomPRG()
+
+	numRecords := 10000
+	data := make([][]byte, numRecords)
+	for i := range data {
+		d := make([]byte, 32)
+		rng.Read(d)
+		data[i] = d
+	}
+	hash := NewBLAKE3()
+
+	branchesLen := int(math.Exp2(math.Ceil(math.Log2(float64(len(data))))))
+
+	// map with the original data to easily loop up the index
+	md := make(map[uint32]uint32, len(data))
+	// We pad our data length up to the power of 2
+	nodes := make([][]byte, branchesLen+len(data)+(branchesLen-len(data)))
+	// Leaves
+	for i := range data {
+		ib := indexToBytes(i)
+		nodes[i+branchesLen] = hash.Hash(data[i], ib)
+
+		checksum := adler32.Checksum(data[i])
+		if md[checksum] != 0 {
+			t.Fatal("collision in checksum output for index ", i)
+		}
+		md[checksum] = uint32(i)
 	}
 }
--- lib/merkle/proof_test.go
+++ lib/merkle/proof_test.go
@@ -1,16 +1,46 @@
 package merkle
 
 import (
-	"math/rand"
 	"testing"
 
 	"github.com/si-co/vpir-code/lib/utils"
 	"github.com/stretchr/testify/require"
 )
 
-func TestEncodeDecodeProof(t *testing.T) {
-	rng := utils.RandomPRG()
-	data := make([][]byte, rand.Intn(501))
+// func TestEncodeDecodeProof(t *testing.T) {
+// 	rng := utils.RandomPRG()
+// 	data := make([][]byte, rand.Intn(501))
+// 	for i := range data {
+// 		d := make([]byte, 32)
+// 		rng.Read(d)
+// 		data[i] = d
+// 	}
+
+// 	// create the tree
+// 	tree, err := New(data)
+// 	require.NoError(t, err)
+
+// 	// generate a proof for random element
+// 	proof, err := tree.GenerateProof(data[rand.Intn(len(data))])
+// 	require.NoError(t, err)
+
+// 	// encode the proof
+// 	b := EncodeProof(proof)
+
+// 	// decode proof
+// 	p := DecodeProof(b)
+
+// 	require.Equal(t, *proof, *p)
+// }
+
+// Test Proof encoding, decoding and verification of each data item
+func TestProofVerification(t *testing.T) {
+	// fix key for debugging purposes
+	rng := utils.NewPRG(&utils.PRGKey{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	//rng := utils.RandomPRG()
+
+	numRecords := 10000
+	data := make([][]byte, numRecords)
 	for i := range data {
 		d := make([]byte, 32)
 		rng.Read(d)
@@ -21,15 +51,23 @@ func TestEncodeDecodeProof(t *testing.T) {
 	tree, err := New(data)
 	require.NoError(t, err)
 
-	// generate a proof for random element
-	proof, err := tree.GenerateProof(data[rand.Intn(len(data))])
-	require.NoError(t, err)
+	// generate a proof for EVERY element
+	for i := range data {
+		proof, err := tree.GenerateProof(data[i])
+		require.NoError(t, err)
 
-	// encode the proof
-	b := EncodeProof(proof)
+		// Check Encoding for each element
+		// encode the proof
+		b := EncodeProof(proof)
+		// decode proof
+		p := DecodeProof(b)
+		require.Equal(t, *proof, *p)
 
-	// decode proof
-	p := DecodeProof(b)
-
-	require.Equal(t, *proof, *p)
+		// check if proof verifies
+		vrf, err := VerifyProof(data[i], proof, tree.Root())
+		require.NoError(t, err)
+		if !vrf {
+			t.Fatal("Proof with index ", i, " did not verify")
+		}
+	}
 }
